5f67be69727ca5c88b63fe6c8a02e07ec21f2483
Helder <hfernandes238@gmail.com>
2025-07-10 19:03:37 +0200

    Mise à jour methods getAllOrders, getOrdersById, updateOrderStatus,getAllOrders, getOrderByIdByAdmin
    Signed-off-by: Helder <hfernandes238@gmail.com>

--- commandes-service/src/main/java/com/ff/commandes_service/service/AdminService.java
+++ commandes-service/src/main/java/com/ff/commandes_service/service/AdminService.java
@@ -1,5 +1,6 @@
 package com.ff.commandes_service.service;
 
+import com.ff.commandes_service.dto.OrderStatusRequest;
 import com.ff.commandes_service.entity.OrderStatus;
 import com.ff.commandes_service.entity.Orders;
 import com.ff.commandes_service.repository.OrderRepository;
@@ -14,24 +15,32 @@ import java.util.Optional;
 public class AdminService {
     private final OrderRepository orderRepository;
 
-    public Optional<Orders> getOrderById(Long id) {
-        return orderRepository.findById(id);
+    public Optional<Orders> getOrderByIdByAdmin(Long id, Long adminId) {
+        Optional<Orders> order = orderRepository.findById(id)
+        .filter(orders -> orders.getAdminId().equals(adminId));
+        if (order.isEmpty()) {
+            throw new IllegalArgumentException("Order not found with id: " + id + " for admin with id: " + adminId);
+        }
+        return order;
     }
 
-    public Optional<List<Orders>> getAllOrders() {
-        return Optional.ofNullable(Optional.of(orderRepository.findAll()).orElseThrow(() -> new IllegalArgumentException("No orders found")));
+    public List<Orders> getAllOrdersByAdmin(Long adminId) {
+        return  orderRepository.findAllProductByAdminId(adminId);
     }
 
     public long countOrders() {
         return orderRepository.count();
     }
-    public Optional<Orders> updateOrderStatus(Long id, OrderStatus status){
+    public Optional<Orders> updateOrderStatus(Long id,Long admin, OrderStatusRequest orderStatus){
         return Optional.of(orderRepository.findById(id).map(order -> {
-            if (order.getOrderStatus() == OrderStatus.CANCELLED || order.getOrderStatus() == OrderStatus.RETURNED) {
-                throw new IllegalArgumentException("Order cannot be updated to " + status + " after it has been " + order.getOrderStatus());
+            if (!order.getAdminId().equals(admin)) {
+                throw new IllegalArgumentException("Order with id: " + id + " does not belong to admin with id: " + admin);
             }
-            order.setOrderStatus(status);
-            switch (status) {
+            if (order.getOrderStatus() == OrderStatus.CANCELLED || order.getOrderStatus() == OrderStatus.RETURNED) {
+                throw new IllegalArgumentException("Order cannot be updated to " + orderStatus + " after it has been " + order.getOrderStatus());
+            }
+            order.setOrderStatus(orderStatus.getOrderStatus());
+            switch (orderStatus.getOrderStatus()) {
                 case PENDING, VALIDATED -> order.setOrderDate(java.time.LocalDateTime.now());
                 case CANCELLED -> order.setCancelledDate(java.time.LocalDateTime.now());
                 case RETURNED -> order.setReturnedDate(java.time.LocalDateTime.now());
