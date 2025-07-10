c4a6261bd4a76168f8e971e5087fbd1dc163cfd3
Helder <hfernandes238@gmail.com>
2025-07-10 19:09:33 +0200

    Mise à jour OrderRepository
    Signed-off-by: Helder <hfernandes238@gmail.com>

--- commandes-service/src/main/java/com/ff/commandes_service/repository/OrderRepository.java
+++ commandes-service/src/main/java/com/ff/commandes_service/repository/OrderRepository.java
@@ -7,16 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Orders, Long> {
-
-    // Custom query methods can be defined here if needed
-    // For example, to find orders by userId or productId
-    List<Orders> findByUserId(Long userId);
-
-    List<Orders> findByProductId(Long productId);
-
     List<Orders> findByOrderStatus(OrderStatus orderStatus);
-
     long countByOrderStatus(OrderStatus status);
-
-    // Additional methods can be added as required
+    List<Orders> findAllProductByAdminId(Long adminId);
 }
