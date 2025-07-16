d2617622af9352ba4290a94df2f81d73f8f4c3de
Helder <hfernandes238@gmail.com>
2025-07-16 17:51:24 +0200

    Ajout : CustomAccessDeniedHandler, CustomAuthenticationEntryPoint
    Signed-off-by: Helder <hfernandes238@gmail.com>

---
+++ api-gateway/src/main/java/com/ff/api_gateway/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,24 @@
+package com.ff.api_gateway.security;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.server.authorization.ServerAccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class CustomAccessDeniedHandler implements ServerAccessDeniedHandler {
+
+    @Override
+    public Mono<Void> handle(ServerWebExchange exchange, AccessDeniedException denied) {
+        exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
+        exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
+        String body = "{\"error\": \"Access Denied\", \"status\": 403}";
+        return exchange.getResponse().writeWith(Mono.just(
+                exchange.getResponse().bufferFactory().wrap(body.getBytes(StandardCharsets.UTF_8))));
+    }
+}
